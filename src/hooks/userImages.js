0e038b8573677f3a4c87b82ccaf034a3b3738590
yash9263 <yashwant8530@gmail.com>
2021-04-26 15:32:56 +0530

    added unsubscribe

--- src/hooks/userImages.js
+++ src/hooks/userImages.js
@@ -1,27 +1,28 @@
-import react, {useState, useEffect, useContext} from 'react';
-import {projectFirestore} from '../firebase/config';
+import react, { useState, useEffect, useContext } from 'react';
+import { projectFirestore } from '../firebase/config';
 import { firebaseContext } from "../hooks/FirebaseProvider";
 
 const userImages = (collection) => {
     const user = useContext(firebaseContext);
     const [userDocs, setUserDocs] = useState({});
-    
-    useEffect (() => {
-        
+
+    useEffect(() => {
+
         const unsub = user && projectFirestore.collection(collection).doc(user.uid)
             .onSnapshot((doc) => {
-            // console.log(doc.data());
-            let documents = {...doc.data()}
-            // console.log(documents);
-            setUserDocs(documents);
+                // console.log(doc.data());
+                let documents = { ...doc.data() }
+                // console.log(documents);
+                setUserDocs(documents);
 
-            return () => unsub();
-        })
 
-        
+            })
+
+        return () => unsub();
+
     }, [collection])
 
-    return {userDocs};
+    return { userDocs };
 }
 
 export default userImages;
